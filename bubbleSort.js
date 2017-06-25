b9654281b8c3f46dde3bc85a598dd1fde9a52b6c
stovermc <stovermc@gmail.com>
2017-06-25 17:09:06 -0600

    finish bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  var sort = true;
+  while(sort) {
+    sort = false;
+    for(var i = 0; i < array.length-1; i++) {
+      if(array[i] > array[i+1]) {
+        var swap = array[i];
+        array[i+1] = array[i];
+        array[i] = swap;
+        sort = true
+      }
+    }
+  }
+}
+
+module.exports = bubbleSort;
---
+++ test/bubbleSortTest.js
@@ -0,0 +1,10 @@
+const assert  = require('chai').assert
+const bubbleSort = require('../bubbleSort')
+
+describe('bubbleSort functionality', function() {
+  context('bubbleSort function', function() {
+    it('sorts an array of integers', function() {
+      assert.equal(bubbleSort([5, 3, 12, 15, 1], [1, 3, 5, 12, 15]))
+    });
+  });
+});
